03696722ab25ea5c064e4117885e3c9c58b23677
Dima <dimasanov@gmail.com>
2022-06-08 18:50:40 +0300

    Changing back AMD module for now

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
 
 export default {
     input: './libs.ts',
@@ -12,5 +13,6 @@ export default {
         typescript({ sourceMap: false }),
         nodeResolve(),
         commonjs(),
+        babel(),
     ],
 };
