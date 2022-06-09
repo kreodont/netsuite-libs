21db52562668fc64c3b1ad0e2f7a834ff36d37e8
Dima <dimasanov@gmail.com>
2022-06-09 14:21:11 +0300

    Adding momentjs

--- netsuite-libs.ts
+++ netsuite-libs.ts
@@ -3,8 +3,8 @@ import * as Logger from './Logger';
 import * as Files from './Files'
 import {StepsRunner, KitTemplate} from "./StepsRunner";
 // import * as ts_serializable from 'ts-serializable'
-// import * as moment from "moment";
+import * as moment from "moment";
 // import * as sweetalert2 from 'sweetalert2'
 // import * as reflect_metadata from 'reflect-metadata'
-
-export { Helpers, Logger, Files, StepsRunner, KitTemplate};
+// import * as ts_serializable from 'ts-serializable'
+export { Helpers, Logger, Files, StepsRunner, KitTemplate, moment};
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
-import { babel } from '@rollup/plugin-babel';
+// import { babel } from '@rollup/plugin-babel';
 
 export default {
     input: './netsuite-libs.ts',
@@ -10,7 +10,7 @@ export default {
         format: 'amd',
     },
     plugins: [
-        typescript({ sourceMap: false }),
+        typescript({ sourceMap: false, module: 'ES2015'}),
         nodeResolve(),
         commonjs(),
         //babel(),
