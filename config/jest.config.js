f6061ea31a981bc0cd82ac80c8ba961dc5f97575
Dmitrii Masanov <dimasanov@gmail.com>
2023-05-04 15:14:48 +0300

    Updating jest.config.js to work with ts-serializable

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    transform: {
+        "^.+\\.(js|jsx)$": "babel-jest",
+    },
+    transformIgnorePatterns: [
+        "node_modules/(?!ts-serializable)",
+    ],
 };
