63b4236428480388b1d082cbf0838762cc97a674
Dmitrii Masanov <dimasanov@gmail.com>
2024-12-12 13:49:21 +0200

    Added more mocks to N.ts

--- __mocks__/N.ts
+++ __mocks__/N.ts
@@ -33,4 +33,10 @@ const runtime = {
     // Add any required methods or properties of the N/runtime module
 };
 
-export { log, query, url, email, runtime };
+const record = {
+    // Add any required methods or properties of the N/record module
+    load: jest.fn().mockReturnValue('test'),
+    Type: {INVOICE: 'invoice', SALES_ORDER: 'salesorder'},
+};
+
+export { log, query, url, email, runtime, record };
