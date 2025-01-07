cf28a3e0a8ed2258f31fdc4101afc76f7959fdcb
Dmitrii Masanov <dimasanov@gmail.com>
2025-01-07 14:55:14 +0200

    Added FieldType mock for N/ui/serverWidget

---
+++ __mocks__/N_ui_serverWidget.ts
@@ -0,0 +1,29 @@
+enum FieldType {
+    CHECKBOX,
+    CURRENCY,
+    DATE,
+    DATETIMETZ,
+    EMAIL,
+    FILE,
+    FLOAT,
+    HELP,
+    INLINEHTML,
+    INTEGER,
+    IMAGE,
+    LABEL,
+    LONGTEXT,
+    MULTISELECT,
+    PASSPORT,
+    PASSWORD,
+    PERCENT,
+    PHONE,
+    SELECT,
+    RADIO,
+    RICHTEXT,
+    TEXT,
+    TEXTAREA,
+    TIMEOFDAY,
+    URL
+}
+
+export { FieldType };
