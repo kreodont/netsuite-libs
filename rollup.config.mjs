a45e1a3377c7827dec7086c567c5566583d7c706
Dmitrii Masanov <dimasanov@gmail.com>
2025-02-07 11:49:43 +0200

    exceljs added

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,9 +3,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 
 export default {
-    input: 'node_modules/pdf-lib/cjs/index.js',
+    input: 'node_modules/exceljs/dist/exceljs.js',
     output: {
-        file: './pdf-lib.js',
+        file: './exceljs.js',
         format: 'amd', // Use AMD format for compatibility with NetSuite's module system
         sourcemap: false,
     },
