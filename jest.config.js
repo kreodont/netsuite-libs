95f28e194c7aefe6a6d9099d7738601b04f9f5fc
Dmitrii Masanov <dimasanov@gmail.com>
2025-02-20 13:32:33 +0200

    Make sure tests debug working

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,22 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['<rootDir>/__tests__/**/*.+(ts|tsx|js)'],
+    transform: {
+        "^.+\\.(js|jsx)$": "babel-jest",
+    },
+    transformIgnorePatterns: [
+        "node_modules/(?!ts-serializable)",
+    ],
     moduleNameMapper: {
         '^N/log$': '<rootDir>/__mocks__/N_log.ts',
         '^N/query$': '<rootDir>/__mocks__/N_query.ts',
         '^N/url$': '<rootDir>/__mocks__/N_url.ts',
         '^N/email$': '<rootDir>/__mocks__/N_email.ts',
-        '^N/file': '<rootDir>/__mocks__/N_file.ts',
+        '^N/file$': '<rootDir>/__mocks__/N_file.ts',
+        '^N/runtime$': '<rootDir>/__mocks__/N_runtime.ts',
         '^N$': '<rootDir>/__mocks__/N.ts',
+        "^N/ui/message$": "<rootDir>/__mocks__/N_ui_message.ts",
+        "^N/ui/serverWidget$": "<rootDir>/__mocks__/N_ui_serverWidget.ts",
+        '^N/currency$': '<rootDir>/__mocks__/N_currency.ts'
     }
 };
